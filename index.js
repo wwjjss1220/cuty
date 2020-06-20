facc491c8cc7699bf41bf3fd191b8926b2ce81ee
wwjjss1220 <67142278+wwjjss1220@users.noreply.github.com>
2020-06-20 18:02:53 +0900

    cuty upload

--- index.js
+++ index.js
@@ -7,8 +7,104 @@ client.on('ready', () => {
 });
 
 client.on('message', (message) => {
+  if(message.content === '메롱') {
+    message.reply('메롱');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '조롱') {
+    message.reply('삐짐 ㅅㄱ');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '메찌') {
+    message.reply('"찌"');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '동휘는') {
+    message.reply('또롱이지');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '팡규팡규') {
+    message.reply('양팡규');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '귀승') {
+    message.reply('귀(지가득)승(민이)');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '야') {
+    message.reply('뭐');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '오리오리') {
+    message.reply('메오리');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '-엄') {
+    message.reply('(마가) 준(비해준) 식(사)');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '메롱이') {
+    message.reply('"너"');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '여기서 가장 귀요미는') {
+    message.reply('누가봐도 "나"지');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '혁스혁스') {
+    message.reply('짱혁스');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '너 메롱이 ㅅㄱ') {
+    message.reply('귀요미가 뭐라는건지 모르겠네>?');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '하') {
+    message.reply('하');
+  }
+});
+
+client.on('message', (message) => {
   if(message.content === 'ㅁㄹ') {
-    message.reply('ㅈㅎ');
+    message.reply('ㅃㅈ');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '우리 빵민아가 제일 예쁘긴 하지') {
+    message.reply('ㅇㅈ');
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content === '메롱이들이 뭐라는거야') {
+    message.reply('너?');
   }
 });
 
